b919bf68aa3c0d76021ed609074c70098a46c2c0
itsfey <91426620+itsfey@users.noreply.github.com>
2022-03-31 17:23:39 +0300

    Create Lets_Review.cs

---
+++ Lets_Review.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) 
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        
+        int T = Convert.ToInt32(Console.ReadLine());
+        string[] s1 = new string[T];
+
+        int i = 0;    
+        while(i < T)
+        {
+            string s = Console.ReadLine();
+            s1[i++] = s;            
+        }
+
+        i = 0;
+        while(i < T)
+        {
+            char[] c1 = s1[i].ToCharArray();
+            string even = "";
+            string odd = "";
+            for (int j = 0; j < c1.Length; j++)
+            {
+                if (j % 2 == 0) even += c1[j];
+                if (j % 2 != 0) odd += c1[j];
+            }
+            Console.WriteLine(even +  " " + odd);
+            i++;
+        }
+
+    }
+}
